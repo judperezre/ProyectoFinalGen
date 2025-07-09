8e83304d2dc5cb7c5831c5b6a820f6ab7ce194ee
judperezre <168015658+judperezre@users.noreply.github.com>
2025-07-09 09:57:30 -0500

    Creación escena UI (#1)
    - Se agregó la escena del menú con los botones
    
    Co-authored-by: lauramoncat11 <lmoncadavargas@gmail.com>

---
+++ Assets/UI/Scripts/MainManagerMenu.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManagerMenu : MonoBehaviour
+{
+    public GameObject panelComoJugar;
+    public GameObject panelCreditos;
+    public GameObject panelHistoria;
+    public GameObject canvasImage;
+    public GameObject canvasMainMenu;
+    public void Jugar()
+    {
+        SceneManager.LoadScene("GameScene"); // reemplaza por el nombre de la escena del juego
+    }
+
+    public void ComoJugar()
+    {
+        panelComoJugar.SetActive(true);
+        canvasImage.SetActive(false);
+        canvasMainMenu.SetActive(false);
+    }
+    public void OcultarComoJugar()
+    {
+        panelComoJugar.SetActive(false);
+        canvasImage.SetActive(true);
+        canvasMainMenu.SetActive(true);
+    }
+    public void Historia()
+    {
+        panelHistoria.SetActive(true);
+        canvasImage.SetActive(false);
+        canvasMainMenu.SetActive(false);
+    }
+
+    public void OcultarHistoria()
+    {
+        panelHistoria.SetActive(false);
+        canvasImage.SetActive(true);
+        canvasMainMenu.SetActive(true);
+    }
+
+    public void Creditos()
+    {
+        panelCreditos.SetActive(true);
+        canvasImage.SetActive(false);
+        canvasMainMenu.SetActive(false);
+    }
+    public void OcultarCreditos()
+    {
+        panelCreditos.SetActive(false);
+        canvasImage.SetActive(true);
+        canvasMainMenu.SetActive(true);
+    }
+}
