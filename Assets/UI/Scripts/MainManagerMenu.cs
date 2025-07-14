34238268a5557b967e976074f1bc4e979dec4fec
lauramoncat11 <lmoncadavargas@gmail.com>
2025-07-14 18:44:27 -0500

    Modificaciones
    - En la escena de la casa añadí el portal y un player de prueba
    - Añadí sonido a las botones del menú (modificaciones en los scripts)
    - Añadí assets de partículas

--- Assets/UI/Scripts/MainManagerMenu.cs
+++ Assets/UI/Scripts/MainManagerMenu.cs
@@ -1,3 +1,4 @@
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,16 +9,22 @@ public class MainManagerMenu : MonoBehaviour
     public GameObject panelHistoria;
     public GameObject canvasImage;
     public GameObject canvasMainMenu;
+
     public void Jugar()
     {
-        SceneManager.LoadScene("GameScene"); // reemplaza por el nombre de la escena del juego
+        AudioManager.Instance.PlayPlaySound(); // ← sonido especial solo para Jugar
+        //StartCoroutine(CambiarPanelConDelay(setupPanel));
+        SceneManager.LoadScene("Enemies"); // reemplaza por el nombre de la escena del juego
     }
 
     public void ComoJugar()
     {
-        panelComoJugar.SetActive(true);
-        canvasImage.SetActive(false);
-        canvasMainMenu.SetActive(false);
+        AudioManager.Instance.PlayClick();
+        StartCoroutine(CambiarPanelConDelay(panelComoJugar));
+
+        //panelComoJugar.SetActive(true);
+        //canvasImage.SetActive(false);
+        //canvasMainMenu.SetActive(false);
     }
     public void OcultarComoJugar()
     {
@@ -27,9 +34,11 @@ public class MainManagerMenu : MonoBehaviour
     }
     public void Historia()
     {
-        panelHistoria.SetActive(true);
+        AudioManager.Instance.PlayClick();
+        StartCoroutine(CambiarPanelConDelay(panelHistoria));
+        /*panelHistoria.SetActive(true);
         canvasImage.SetActive(false);
-        canvasMainMenu.SetActive(false);
+        canvasMainMenu.SetActive(false);*/
     }
 
     public void OcultarHistoria()
@@ -41,14 +50,25 @@ public class MainManagerMenu : MonoBehaviour
 
     public void Creditos()
     {
-        panelCreditos.SetActive(true);
+        AudioManager.Instance.PlayClick();
+        StartCoroutine(CambiarPanelConDelay(panelCreditos));
+        /*panelCreditos.SetActive(true);
         canvasImage.SetActive(false);
-        canvasMainMenu.SetActive(false);
+        canvasMainMenu.SetActive(false);*/
     }
     public void OcultarCreditos()
     {
         panelCreditos.SetActive(false);
         canvasImage.SetActive(true);
         canvasMainMenu.SetActive(true);
+    }
+
+    IEnumerator CambiarPanelConDelay(GameObject panelNuevo)
+    {
+        yield return new WaitForSeconds(0.1f); // Espera un poco para que suene el clic
+        //mainPanel.SetActive(false);
+        canvasImage.SetActive(false);
+        canvasMainMenu.SetActive(false);
+        panelNuevo.SetActive(true);
     }
 }
