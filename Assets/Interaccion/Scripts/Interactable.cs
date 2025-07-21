e484627d641ca28257552abee04c474a99893b95
Wilsonad11 <wilsonsilva06361@hotmail.com>
2025-07-21 09:47:13 -0500

    DanielChanges5 (#21)
    * se crea logica y comportamiento del enemigo
    
    * PlayerMovement
    
    * Se crean la AI de los enemigos de rango y melee
    
    * Nuevocommit
    
    * new1
    
    * Se agrega asset básico y animación de camoinar para enemigos de rango
    
    * NewChangesDani
    
    * Se arreglan bugs de eemigos se agregan animaciones
    
    * NewChangesDaniS
    
    * ChangesDaniS
    
    * NewchangesDaniS1
    
    * Newchangesdani2
    
    * dANIELCHANGES2
    
    * ChangesDani3
    
    * Changesdanis4
    
    * PrefinalchangesDaniel
    
    ---------
    
    Co-authored-by: judperezre <168015658+judperezre@users.noreply.github.com>

---
+++ Assets/Interaccion/Scripts/Interactable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+
+    Outline outline;
+    public string message;
+
+    public UnityEvent onInteraction;
+    
+    void Start()
+    {
+        outline = GetComponent<Outline>();
+        DisableOutline();
+    }
+
+    public void Interact()
+    {
+        onInteraction.Invoke();
+    }
+    public void DisableOutline()
+    {
+        outline.enabled = false;
+    }
+
+    public void EnableOutline()
+    {
+        outline.enabled = true;
+    }
+}
